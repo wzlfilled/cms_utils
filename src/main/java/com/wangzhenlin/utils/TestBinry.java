c7bc98b5fb9f3f91d3bd13798b92e4c2991fe8ce
wzlfilled <1334171844@qq.com>
2019-12-06 20:02:55 +0800

    工具类

---
+++ src/main/java/com/wangzhenlin/utils/TestBinry.java
@@ -0,0 +1,20 @@
+package com.wangzhenlin.utils;
+
+public class TestBinry {
+
+	public static void main(String[] args) {
+		
+		int a = 12;// 10进制
+		int b = 012;// 8进制
+		int c = 0x12;// 16进制
+		int d = 0b1101;// 2进制
+		
+		System.out.println("a"+a);
+		System.out.println("b"+b);
+		System.out.println("c"+c);
+		System.out.println("d"+d);
+		
+		
+	}
+	
+}
